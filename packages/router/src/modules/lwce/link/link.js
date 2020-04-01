ece0d881a74181cd4a30d8a735a95a44503867c8
Bret Little <blittle@salesforce.com>
2020-04-01 13:31:10 -0400

    Fix class -> className

--- packages/router/src/modules/lwce/link/link.js
+++ packages/router/src/modules/lwce/link/link.js
@@ -3,7 +3,7 @@ import {LightningElement, api} from 'lwc';
 export default class Link extends LightningElement {
   @api title;
   @api href;
-  @api class;
+  @api className;
   @api role;
   @api ariaPressed;
 
